00e4e2e4f287c7dfd31458cb6e3b9fe16602a3ed
VAdam02 <adam.vilanyi2@gmail.com>
2024-09-23 18:57:19 +0200

    Code with bug

--- gyak3/script1.js
+++ gyak3/script1.js
@@ -7,6 +7,13 @@ addEventListener("load", () => {
     /*
    const linkDiv = document.querySelector("div").addEventListener("click", blockNonElteLinks2)
    */
+
+   document.querySelectorAll(".left").forEach(element => element.addEventListener("input", copyContent))
+
+
+   addEventListener("click", (
+        (alma) => copyContent(alma))
+    );
 })
 
 function blockNonElteLinks(event) {
@@ -31,6 +38,17 @@ function blockNonElteLinks2(event) {
         return;
     }
 }
+
+function copyContent(event) {
+    console.log(event);
+    if (event.target.tagName !== "INPUT") return;
+
+    console.log({asd: event.target})
+    console.log(event.target.parentNode.childNodes[2])
+    event.target.parentNode.childNodes[2].value="24"
+}
+
+
 
 /*
 addEventListener("click", (event) => {
