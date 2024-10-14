386afd0aec9a9f65f88bb943a079465c8130e66f
VAdam02 <adam.vilanyi2@gmail.com>
2024-10-14 18:39:52 +0200

    Plane moving

--- gyak6/script2.js
+++ gyak6/script2.js
@@ -4,8 +4,44 @@ let ctx;
 addEventListener("load", () => {
     canvas = document.querySelector("canvas");
     ctx = canvas.getContext("2d");
-    ctx.drawImage(plane.img, plane.x, plane.y, plane.width, plane.height)
+
+    canvas.addEventListener("click", () => {
+        if (gameState == 0) {
+            gameState = 1;
+        }
+    })
+
+    next();
 })
+
+let lastFrame = performance.now();
+function next() {
+    const deltatime = (performance.now() - lastFrame) / 1000; //in seconds
+    lastFrame = performance.now();
+
+    update(deltatime);
+    render();
+
+    requestAnimationFrame(next);
+}
+
+let gameState = 0;
+
+function update(deltatime) {
+    if (gameState == 0) {
+        console.log("Waiting for click")
+    } else if (gameState == 1) {
+        plane.vx = 200;
+    }
+
+    plane.x += plane.vx * deltatime;
+}
+
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.drawImage(plane.img, plane.x, plane.y, plane.width, plane.height)
+}
 
 const plane = {
     x: 0,
