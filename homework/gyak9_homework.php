37e21ebb3114530c72c3fcef70dfec51a09f3f6c
VAdam02 <adam.vilanyi2@gmail.com>
2024-11-14 22:28:06 +0100

    Gyak 9 homework

---
+++ homework/gyak9_homework.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Az órai regisztrációs űrlap alá hozz létre egy új űrlapot, melynek feladata, hogy a már regisztrált felhasználó meg tudja változtatni a jelszavát.
+felhasználónév: kötelező, szöveg, létezik a felhasználók között
+jelszó: kötelező, szöveg, minimum 8 karakter
+jelszó újra: kötelező, szöveg, minimum 8 karakter, megegyezik az előző mezővel
+
+Beadáskor az órai kódod is küldd el, mert önálló munkával lett létrehozva, így az egyes megoldások eltérhetnek egymástól, például a tárolt adatszerkezet mezőinek nevei, amely megnehezíti a tesztelés folyamatát
+*/
+?>
