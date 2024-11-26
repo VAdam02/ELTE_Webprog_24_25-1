96a3972b8b2dba2a5b3e18786c8b2d70304f273e
VAdam02 <adam.vilanyi2@gmail.com>
2024-11-26 18:43:55 +0100

    Gyak 11 homework

---
+++ homework/gyak11_homework.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Az órai kódot egészítsétek ki egy regisztrációs oldallal egy új php fájlban.
+
+Elvárt bemenetek:
+email: email, kötelező
+felhasználónév: szöveg, kötelező, egyedi, legalább 3 karakter
+jelszó: szöveg, kötelező, legalább 5 karakter
+jelszó megerősítése: szöveg, kötelező, megegyezik a jelszóval
+
+Sikeres felküldés és validálás után tároljuk el jsonben a többi felhasználó mellé, majd irányítsuk át a kezdőlapra.
+*/
+?>
